7d4698ad06918786fdb7dd7ac93065d0d69d7ad4
Timurinyo <timurinyo@gmail.com>
2020-10-15 18:51:33 +0300

    Update app.py
    Minimal clean up

--- app.py
+++ app.py
@@ -6,24 +6,25 @@ import datetime
 
 locale.setlocale(locale.LC_ALL, '')
 
-token = 'uR-j44ioFTArzlrFd6JJXyLAZH7vIUf4kEG-H4xmjSMM'
-white_card_id = 'gsSGMNasjjQR_2BDIVOv3g'
+TOKEN = '*'
+WHITE_CARD_ID = 'gsSGMNasjjQR_2BDIVOv3g'
 
 FIRST_TIME = True
 
 UPLOAD_FOLDER = 'static/uploads'
 
 BALANCE = 0
-PEOPLE_DONATED = 0
+PEOPLE_DONATED_AMOUNT = 0
 PEOPLE_DONATED_NAMES = ''
 START_DONATION_DATE = datetime.datetime(2020, 9, 13)
 LAST_REQUEST_TIME = 0
+REQUEST_PERIOD = 120
 
 app = Flask(__name__)
 app.config.from_pyfile('config.py')
 
 
-mono = monobank.Client(token)
+mono = monobank.Client(TOKEN)
 
 def find_card(card_id, user_info):
     for card in user_info['accounts']:
@@ -55,7 +56,6 @@ def get_people_donated(card_id, since, to=datetime.datetime.now()):
 
 def format_descriptions(descriptions):
     filtered_descriptions = [d.lstrip('Від: ') for d in descriptions if d != 'З гривневої картки']
-    filtered_descriptions.append('Илья Елагин')
     return filtered_descriptions
 
 @app.route("/")
@@ -63,19 +63,19 @@ def home():
     global FIRST_TIME
     global LAST_REQUEST_TIME
     global BALANCE
-    global PEOPLE_DONATED
+    global PEOPLE_DONATED_AMOUNT
     global PEOPLE_DONATED_NAMES
 
     current_time = int(time.time())
     time_difference = current_time - LAST_REQUEST_TIME
-    print(time_difference)
-    if time_difference > 121 or FIRST_TIME:
-        gethered = get_account_balance(white_card_id)
+
+    if time_difference > REQUEST_PERIOD or FIRST_TIME:
+        gethered = get_account_balance(WHITE_CARD_ID)
         BALANCE = gethered
 
-        descriptions, people_supported = get_people_donated(white_card_id, START_DONATION_DATE)
+        descriptions, people_supported_amount = get_people_donated(WHITE_CARD_ID, START_DONATION_DATE)
 
-        PEOPLE_DONATED = people_supported
+        PEOPLE_DONATED_AMOUNT = people_supported_amount
 
         supporters_names = format_descriptions(descriptions)
         supporters_names_string = ', '.join(supporters_names)
@@ -84,18 +84,22 @@ def home():
         LAST_REQUEST_TIME = int(time.time())
     else:
         gethered = BALANCE
-        people_supported = PEOPLE_DONATED
+        people_supported_amount = PEOPLE_DONATED_AMOUNT
         supporters_names_string = PEOPLE_DONATED_NAMES
 
     time_until_birthday = datetime.datetime(2020, 9, 29) - datetime.datetime.now()
-    days_until_birthday = time_until_birthday.days+1
+    days_until_birthday = time_until_birthday.days + 1
 
-    gethered = gethered + 56240
-
-    progress_bar_style_width = f"style=width:{(gethered / 300000)*100}%"
+    progress_bar_style_width = f"style=width:{ (gethered / 300000) * 100 }%"
 
     FIRST_TIME = False
-    return render_template("index.html", gathered="{0:n}".format(gethered).replace(',', ' '), people_supported=people_supported, days_until_birthday=days_until_birthday, progress_bar_style_width=progress_bar_style_width, supporters_names='Поздравили: ' + supporters_names_string)
+    return render_template("index.html", 
+                           gathered="{0:n}".format(gethered).replace(',', ' '), 
+                           people_supported=people_supported_amount, 
+                           days_until_birthday=days_until_birthday, 
+                           progress_bar_style_width=progress_bar_style_width, 
+                           supporters_names='Поздравили: ' + supporters_names_string
+                          )
 
 @app.route("/support")
 def support():
